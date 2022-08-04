9732a83e07df17b014a1a934269c681703c54c1d
Marco Lau <lau.career@gmail.com>
2022-08-04 23:47:33 +0800

    Add eks module

--- main.tf
+++ main.tf
@@ -17,3 +17,29 @@ module "vpc" {
   enable_dhcp_options   = true
 }
 
+module "eks" {
+  source          = "terraform-aws-modules/eks/aws"
+  version         = "18.23.0"
+  cluster_name    = var.cluster_name
+  cluster_version = var.cluster_version
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_group_defaults = {
+    ami_type               = "AL2_x86_64"
+    disk_size              = 50
+    instance_types         = ["t3.medium"]
+  }
+
+  eks_managed_node_groups = {
+    eks_nodes = {
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+    }
+  }
+
+  enable_irsa = true
+}
+
